3796a7634937e47d86f838c6729fd5183f87c3bf
Penguin <son@vdburg.site>
2025-04-10 18:17:24 +0200

    animations and images added

--- script.js
+++ script.js
@@ -33,17 +33,57 @@ async function getLoreData() {
  * Print the lore and the lore title of the specified era on the web page
  * @param {number} index which era of lore to print
  */
-function printLore(data, index = 0) {
+async function printLore(data, index = 0) {
     const loreTitleElement = document.getElementById("loreTitle");
     const loreTextElement = document.getElementById("loreText");
+    const bodyElement = document.body;
 
     const eraData = data[index];
 
-    loreTitleElement.innerText = eraData.title;
-    loreTextElement.innerText = eraData.text;
+    // Set background
+    bodyElement.style.backgroundImage = `url(images/${eraData.backgroundImageURL})`;
+
+    // Begin animation
+    loreTitleElement.classList.add("slideOut");
+    loreTextElement.classList.add("slideOut");
+
+    // Wait until animation is finished, then set the new text
+    setTimeout(() => {
+        loreTitleElement.innerText = eraData.title;
+        loreTextElement.innerText = eraData.text;
+
+        loreTitleElement.classList.remove("slideOut");
+        loreTextElement.classList.remove("slideOut");
+        loreTitleElement.classList.add("slideIn");
+        loreTextElement.classList.add("slideIn");
+    }, 400);
+
     console.debug("Lore printed on the web page");
 }
 
+const rangeElement = document.getElementById("timeline");
+let rangeValue = rangeElement.value;
+const rangeMax = rangeElement.max;
+
 getLoreData().then((data) => {
     printLore(data);
+
+    const totalEras = data.length - 1;
+    const eraLength = Math.floor(rangeMax / (totalEras + 1));
+    console.debug("Total eras: ", totalEras, "| Era length: ", eraLength);
+
+    let currentEra = Math.floor(rangeValue / eraLength);
+
+    // Range slider listener
+    const rangeSlider = rangeElement;
+    rangeSlider.addEventListener("input", () => {
+        rangeValue = rangeElement.value;
+
+        // Check if the current era has changed
+        if (Math.floor(rangeValue / (eraLength + 1)) !== currentEra) {
+            currentEra = Math.floor(rangeValue / eraLength);
+            console.debug("Lore changed to era: ", currentEra);
+            printLore(data, currentEra);
+        }
+    });
 });
